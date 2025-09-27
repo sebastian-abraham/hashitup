0d51dbf2b5b9f11f1b5c1c2a7984521e7ed02628
JephilReji <jephilr2@gmail.com>
2025-09-27 10:24:27 +0530

    pre vercel fix (vercel.json)

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script>
-  import { slide } from "svelte/transition";
-  import { page } from "$app/stores";
-  import { quintOut } from "svelte/easing";
 </script>
 
-{#key $page.url.pathname}
-  <div>
-    <slot />
-  </div>
-{/key}
+<svelte:head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <style>
+        body { font-family: 'Inter', sans-serif; }
+    </style>
+</svelte:head>
+
+<slot />
